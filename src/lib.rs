cd431f00fc182b7426de482fd171a03d1cf1742f
stackswithans <stexor12@gmail.com>
2021-04-17 14:11:20 +0100

    Fix paths

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-pub mod ds{
+pub mod ds {
+    mod doubly_linked_list;
     mod linked_list;
-    mod dlinked_list;
+    pub use doubly_linked_list::DoublyLinkedList;
     pub use linked_list::LinkedList;
 }
